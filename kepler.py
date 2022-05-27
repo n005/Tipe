880ea17f9a6f693cb310e268f223543df277f33b
n005 <n005char@gmail.com>
2022-05-27 22:29:10 +0200

    Add cursor previz

--- kepler.py
+++ kepler.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d
+from matplotlib.widgets import Slider, Button
 
 @dataclass
 class Kepler:
@@ -83,6 +84,62 @@ def plot3d_wthsat(*kepler):
         tmp2 = cartesian_3d(i[0], i[1])
         ax.scatter(tmp2[0], tmp2[1], tmp2[2], s=70)
     plt.show()
+    
+    
+
+def cursor():  
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    axa = plt.axes([0.25, 0.15, 0.65, 0.03])
+    axe = plt.axes([0.25, 0.1, 0.65, 0.03])
+    axi = plt.axes([0.25, 0.2, 0.65, 0.03])
+    axomega = plt.axes([0.25, 0.25, 0.65, 0.03])
+    axpomega = plt.axes([0.25, 0.30, 0.65, 0.03])
+    axsat = plt.axes([0.25, 0.35, 0.65, 0.03])
+    
+    aax = Slider(axa, 'Semimajor Axis', 35000000, 37000000, 36000000)
+    eax = Slider(axe, 'Eccentricity', 0.0, 1, 0)
+    iax = Slider(axi, 'Inclination', 0.0, 2*np.pi, 0)
+    omegaax = Slider(axomega, 'Longitude of the ascending node', 0.0, 2*np.pi, 0)
+    pomegaax = Slider(axpomega, 'Argument of periapsis', 0.0, 2*np.pi, 0)
+    satax = Slider(axsat, 'Eccentric anomaly', 0.0, 2*np.pi, 0)
+    
+    x = np.linspace(-5*np.pi, 5*np.pi, 500)
+    ax.scatter([0], [0], [0], color="g", s=100)
+    tmp = cartesian_3d(Kepler(36000000,0,0,0,0), x)
+    l = ax.plot(tmp[0], tmp[1], tmp[2], linewidth=0.5)
+    tmp22 = cartesian_3d(Kepler(36000000,0,0,0,0), 0)
+    ax.scatter(tmp22[0], tmp22[1], tmp22[2], s=70)
+    ax.set_xlim3d(-3e7, 3e7)
+    ax.set_ylim3d(-3e7, 3e7)
+    ax.set_zlim3d(-3e7, 3e7)
+    
+    def cursor_update(val):
+        a = aax.val
+        e = eax.val
+        i = iax.val
+        omega = omegaax.val
+        pomega = pomegaax.val
+        sat = satax.val
+        tmp2 = cartesian_3d(Kepler(a,e,i,omega,pomega), x)
+        tmp3 = cartesian_3d(Kepler(a,e,i,omega,pomega), sat)
+        ax.clear()
+        ax.plot(tmp2[0], tmp2[1], tmp2[2], linewidth=0.5)
+        ax.scatter([0], [0], [0], color="g", s=100)
+        ax.scatter(tmp3[0], tmp3[1], tmp3[2], s=70)
+        ax.set_xlim3d(-3e7, 3e7)
+        ax.set_ylim3d(-3e7, 3e7)
+        ax.set_zlim3d(-3e7, 3e7)
+    
+    aax.on_changed(cursor_update)
+    eax.on_changed(cursor_update)
+    iax.on_changed(cursor_update)
+    omegaax.on_changed(cursor_update)
+    pomegaax.on_changed(cursor_update)
+    satax.on_changed(cursor_update)
+
+cursor()
+    
 
 a=36000000
 e=0
@@ -90,4 +147,4 @@ i=0
 omega=0
 pomega=0
 
-plot3d_wthsat([Kepler(a,e,i,omega,pomega), -np.pi/3], [Kepler(a,e,0.2,omega,pomega), np.pi/2])
+#plot3d_wthsat([Kepler(a,e,i,omega,pomega), -np.pi/3], [Kepler(a,e,0.2,omega,pomega), np.pi/2])
