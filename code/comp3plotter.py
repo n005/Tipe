9b798bd4c7d92d2705bfd715a19817ccf03d0b83
n005 <n005char@gmail.com>
2022-11-13 19:03:16 +0100

    Update comp3plotter.py

--- code/comp3plotter.py
+++ code/comp3plotter.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
 import matplotlib.pyplot as plt
+from COMPACT3 import compread
 
-colnames=['TIME', 'PRN', 'misure'] 
-df = pd.read_csv('C:/Users/n005/Documents/TIPE/data/datatest/t.txt', names=colnames, header=None)
-
-val = df[df['PRN'] != 1006]
+val=compread('C:/Users/n005/Documents/TIPE/data/datatest/obs.m51')
+val = val[val['PRN'] != 'G06']
 
 plt.scatter(val['TIME'], val['misure'])
 plt.title("Multipath error")
-plt.xlabel("Time (s)")
+plt.xlabel("Time")
 plt.ylabel("Error (m)")
